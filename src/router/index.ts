ae39e9184e65c4264e91732030b5081b68b92d9b
David Moskalenko <dvdmsk21@gmail.com>
2025-06-27 23:30:43 +0300

    sol

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../pages/Home.vue'
-import Property from '../pages/Property.vue'
-import Map from '../pages/Map.vue'
+import { createRouter, createWebHashHistory } from "vue-router";
+import Home from "../pages/Home.vue";
+import Property from "../pages/Property.vue";
+import Map from "../pages/Map.vue";
 
 const routes = [
-  { path: '/', component: Home, },
-  { path: '/property/:id', component: Property },
-  { path: '/map', component: Map },
-]
+  { path: "/", component: Home },
+  { path: "/property/:id", component: Property },
+  { path: "/map", component: Map },
+];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory("/vue-apartment/"),
   routes,
-})
+});
 
-export default router
+export default router;
